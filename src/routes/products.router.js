5e424e512264b45a44f0b3f8582299d2d2738c91
Sergio-Tolaba <tolser2019@gmail.com>
2025-05-04 18:37:08 -0300

    otra ruta que busca id=>..productos/:id (url+req.params.id)

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+//nva ruta /productos desde fakesotreapi
 router.get("/productos", (req, res) => {
   fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
     .then((productos) => res.send(productos));
+});
+//buscar 1 producto fakestoreapi x id=>/productos/:idapi
+router.get("/productos/:id", (req, res) => {
+  fetch("https://fakestoreapi.com/products/" + req.params.id)
+    .then((res) => res.json())
+    .then((producto) => res.send(producto));
 });
 
 module.exports = router;
