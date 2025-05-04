e724dc1104a0182d9b3e17b3219e68c96d709fd4
Sergio-Tolaba <tolser2019@gmail.com>
2025-05-04 19:53:38 -0300

    nva ruta interfería con product?limit=2, condicional op ternario

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,16 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 //nva ruta /productos desde fakesotreapi
-router.get("/productos", (req, res) => {
-  fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
-    .then((productos) => res.send(productos));
-});
+// router.get("/productos", (req, res) => {
+//   fetch("https://fakestoreapi.com/products")
+//     .then((res) => res.json())
+//     .then((productos) => res.send(productos));
+// });
 //buscar 1 producto fakestoreapi x id=>/productos/:idapi
 router.get("/productos/:id", (req, res) => {
   fetch("https://fakestoreapi.com/products/" + req.params.id)
     .then((res) => res.json())
     .then((producto) => res.send(producto));
 });
-
+//limitar busqueda con query
+router.get("/productos", (req, res) => {
+  const limit = req.query.limit;
+  const url = limit
+    ? `https://fakestoreapi.com/products?limit=${limit}`
+    : `https://fakestoreapi.com/products`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((productos) => res.send(productos));
+});
 module.exports = router;
