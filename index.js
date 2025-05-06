0df48ab64b69063a5ae10f6f73cdef8def83f627
Sergio-Tolaba <tolser2019@gmail.com>
2025-05-06 09:52:21 -0300

    folders private(index.html) and public(css/style.css=>express.static)

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
+//middleware
+app.use(express.static(path.join(__dirname, "public")));
 //Routes Ej principal la llevé a src/routes/main.router.js, luego la llamo y la uso
 const mainRouter = require("./src/routes/main.router");
 app.use(mainRouter);
-//prefijo:'/productos' simplifica las rutas
+//prefijo:'/productos' simplifica las rutas y los dos líneas anteriores las resumo en una
 app.use("/productos", require("./src/routes/products.router"));
 
 const PORT = process.env.PORT || 3001;
-//server y usos
 
 app.listen(PORT, () => {
   console.log(`Server run on http://localhost:${PORT}`);
--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,5 +1,9 @@
+const path = require("path");
 const index = (req, res) => {
-  res.send("Hola Mundo!!!");
+  //res.send("Hola Mundo!!!"); Texto plano pero quiero un html y luego un html dinámico(.ejs)
+  //res.sendFile("../../private/index.html");=>TypeError: path must be absolute or specify root to res.sendFile=>requerir path
+  res.sendFile(path.resolve("__dirname,../../private/index.html")); //resuelve la ruta
+  //console.log(__dirname); // muestra la ruta en la cual estoy (relativa=>absoluta-dinámica)
 };
 
 module.exports = {
